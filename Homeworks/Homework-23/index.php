ffeb47772d6ec4d77f22f1acdf10c1ac06033673
mikayel <mikayel.margaryan@gmail.com>
2016-07-09 11:19:51 +0400

    Homework 23 - ACA DB admin

--- Homeworks/Homework-23/index.php
+++ Homeworks/Homework-23/index.php
@@ -88,10 +88,8 @@ if(isset($_GET['id'])){
     <title>ACA</title>
 
     <!-- Bootstrap -->
-    <!-- build
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    -->
-    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="CSS/style.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -252,10 +250,7 @@ if(isset($_GET['id'])){
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <!-- build
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    -->
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="JS/script.js"></script>
 </body>
 </html>
