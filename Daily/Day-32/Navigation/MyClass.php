92de348fcc3599eaac9876ab08aba735adb9206d
mikayel <mikayel.margaryan@gmail.com>
2016-07-31 11:11:07 +0400

    Why you need namespaces

---
+++ Daily/Day-32/Navigation/MyClass.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: mikayel
+ * Date: 7/31/16
+ * Time: 10:58 AM
+ */
+
+namespace Navigation;
+
+class MyClass
+{
+
+}
---
+++ Daily/Day-32/index.php
@@ -0,0 +1,10 @@
+<?php
+
+  require_once "Navigation/MyClass.php";
+  require_once "Pagination/MyClass.php";
+
+    use Navigation\MyClass as N_class;
+    use Pagination\MyClass as P_class;
+
+    $obj = new N_class();
+    $obj2 = new P_class();
