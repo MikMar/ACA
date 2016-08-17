b5819804df793d655f8ab8f0281f82a1313b7e02
mikayel <mikayel.margaryan@gmail.com>
2016-08-17 19:51:38 +0400

    All you need to know about Bcrypt

---
+++ Daily/Day-40/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mikayel
+ * Date: 8/17/16
+ * Time: 7:20 PM
+ */
+
+$options = [
+    'cost' => 5
+];
+
+$timeBefore = microtime();
+
+$hash1 = password_hash('123456', PASSWORD_BCRYPT, $options);
+//$hash2 = password_hash('123456', PASSWORD_BCRYPT);
+
+var_dump(password_verify('123456', $hash1));
+//password_needs_rehash();
+var_dump(password_get_info($hash1));
+
+$timeAfter = microtime();
+echo $timeAfter - $timeBefore;
